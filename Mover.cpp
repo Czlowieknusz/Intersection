cd4eb700a9a6c9b286dbf04697e8c5e7a986fd88
Jan Woźniak <jnwznk@gmail.com>
2019-06-11 07:05:21 +0200

    Direction right is also handled.

--- Mover.cpp
+++ Mover.cpp
@@ -9,10 +9,14 @@ void Mover::moveCars(std::list<std::shared_ptr<Car>> &cars) {
     for (auto car = cars.begin(); car != cars.end(); ++car) {
         switch (car->get()->getDirection()) {
             case Direction::TOP:
-                car->get()->moveUp();
+                if (checkIfFreeToMove(car, cars)) {
+                    car->get()->moveUp();
+                }
                 break;
             case Direction::BOTTOM:
-                car->get()->moveDown();
+                if (checkIfFreeToMove(car, cars)) {
+                    car->get()->moveDown();
+                }
                 break;
             case Direction::LEFT:
                 if (checkIfFreeToMove(car, cars)) {
@@ -20,7 +24,9 @@ void Mover::moveCars(std::list<std::shared_ptr<Car>> &cars) {
                 }
                 break;
             case Direction::RIGHT:
-                car->get()->moveRight();
+                if (checkIfFreeToMove(car, cars)) {
+                    car->get()->moveRight();
+                }
                 break;
         }
     }
@@ -34,20 +40,54 @@ bool Mover::checkIfFreeToMove(std::list<std::shared_ptr<Car>>::iterator car, std
         case Direction::BOTTOM:
             return true;
         case Direction::LEFT:
-            if (not isSubordinatedClear and car->get()->getCoordY() == intersectionFromRight - 1) {
+            if (not isSubordinatedClear and car->get()->getCoordY() == intersectionFromRight + 1) {
                 return false;
             } else if (car->get()->getCoordY() >= intersectionFromRight) {
                 if (car == cars.begin()) {
                     return true;
                 } else if (car->get()->getCoordY() >= std::prev(car, 1)->get()->getCoordY() + 8) {
                     return true;
+                } else if (car->get()->getCoordY() < std::prev(car, 1)->get()->getCoordY() + 8) {
+                    return false;
                 }
             }
             return true;
         case Direction::RIGHT:
+//            if (car->get()->getCoordY() == intersectionFromLeft - 4)
+//                return false;
+            if (not isSubordinatedClear and car->get()->getCoordY() == intersectionFromLeft - 4) {
+                return false;
+            } else if (car->get()->getCoordY() <= intersectionFromLeft-4) {
+                if (car == cars.begin()) {
+                    return true;
+                } else if (car->get()->getCoordY() <= std::prev(car, 1)->get()->getCoordY() - 8) {
+                    return true;
+                } else if (car->get()->getCoordY() > std::prev(car, 1)->get()->getCoordY() - 8) {
+                    return false;
+                }
+            }
             return true;
     }
 }
+/*
+ *
+ * C = animateSquare(0, 0, 2, 15);
+ * __________________________________________> Y
+ * |
+ * |
+ * | (C)|||||||||||||
+ * |    |||||||||||||
+ * |
+ * |
+ * |  (10, 2)
+ * |   X
+ * |
+ * |
+ * |
+ * |
+ * \/
+ * X
+ */
 
 void Mover::checkIfIntersectionClear(std::list<std::shared_ptr<Car>> &cars) {
     for (auto &car : cars) {
